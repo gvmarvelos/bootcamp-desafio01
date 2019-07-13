cc23a7584e53a9aa2d5eaf1cb4d4a31c1eecfd58
gustavo <gvmarvelos@gmail.com>
2019-07-13 15:51:34 -0300

    chore: Construcao das rotas

--- index.js
+++ index.js
@@ -6,16 +6,32 @@ server.use(express.json());
 
 const projects = [];
 
-function verifyProjectIDExist() {}
+let totalRequests = 0;
 
-function countAllRequests() {}
+function verifyProjectIDExist(req, res, next) {
+  const { id } = req.params;
+  for (var i = 0; i < projects.length; i++) {
+    if (projects[i].id == id) {
+      return next();
+    }
+  }
+  return res.status(400).json({
+    error: "O projeto não existe!"
+  });
+}
 
-server.post(
-  "/projects",
-  verifyProjectIDExist,
-  countAllRequests,
-  (req, res) => {}
-);
+function countAllRequests(req, res, next) {
+  totalRequests = totalRequests + 1;
+  console.log(totalRequests);
+
+  return next();
+}
+
+server.post("/projects", countAllRequests, (req, res) => {
+  const { project } = req.body;
+  projects.push(project);
+  return res.json(projects);
+});
 
 server.get("/projects", countAllRequests, (req, res) => {
   return res.json(projects);
@@ -25,19 +41,50 @@ server.put(
   "/projects/:id",
   countAllRequests,
   verifyProjectIDExist,
-  (req, res) => {}
+  (req, res) => {
+    const { id } = req.params;
+    const { title } = req.body;
+
+    for (var i = 0; i < projects.length; i++) {
+      if (projects[i].id == id) {
+        projects[i].title = title;
+        return res.json(projects);
+      }
+    }
+  }
 );
 
 server.delete(
   "/projects/:id",
   countAllRequests,
   verifyProjectIDExist,
-  (req, res) => {}
+  (req, res) => {
+    const { id } = req.params;
+
+    for (var i = 0; i < projects.length; i++) {
+      if (projects[i].id == id) {
+        projects.splice(i);
+        return res.json(projects);
+      }
+    }
+  }
 );
 
 server.post(
   "/projects/:id/tasks",
   countAllRequests,
   verifyProjectIDExist,
-  (res, req) => {}
+  (res, req) => {
+    const { id } = req.params;
+    const { tasks } = req.body;
+
+    for (var i = 0; i < projects.length; i++) {
+      if (projects[i].id == id) {
+        projects[i].tasks.push(tasks);
+        return res.json(projects);
+      }
+    }
+  }
 );
+
+server.listen(3000);
